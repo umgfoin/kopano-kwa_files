07799e9e13b2e99a2eb5b0329491867eb1de8ac6
Martijn Alberts <m.alberts@kopano.com>
2020-05-06 12:15:07 +0200

    class.backend.php fix typo

--- php/Files/Backend/FTP/class.backend.php
+++ php/Files/Backend/FTP/class.backend.php
@@ -923,7 +923,7 @@ class Backend extends AbstractBackend implements iFeatureStreaming
 				break;
 			case self::FTP_ERR_INTERNAL:
 				// This is a general error, might be thrown due to a wrong IP, but we don't know.
-				$msg = dgettext('plugin_files', 'The file server encountered an internal problem. Please contact your system administrator?');
+				$msg = dgettext('plugin_files', 'The file server encountered an internal problem. Please contact your system administrator.');
 				break;
 			case self::FTP_ERR_TMP:
 				$msg = dgettext('plugin_files', 'We could not write to temporary directory. Please contact your system administrator.');
