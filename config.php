81f42de9c09d0279632bce065dd7209edebd4301
Martijn Alberts <m.alberts@kopano.com>
2020-09-08 15:25:07 +0200

    config.php change log level

--- config.php
+++ config.php
@@ -21,7 +21,7 @@ define('PLUGIN_FILES_CACHE_DIR', "/var/lib/kopano-webapp/plugin_files");
  *
  * This setting is not editable within webapp!!
  */
-define('PLUGIN_FILESBROWSER_LOGLEVEL', "DEBUG");
+define('PLUGIN_FILESBROWSER_LOGLEVEL', "ERROR");
 
 /**
  * Standard password key for account data encryption. We recommend to change the default value for security reasons
